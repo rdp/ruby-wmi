7343e4442d2bb8556f0dfabee7184618b200151f
rdp <rogerpack2005@gmail.com>
2009-12-15 10:59:25 -0700

    use jeweler instead of Hoe [which was broken for newer versions of Hoe, and also on windows]

--- lib/ruby-wmi.rb
+++ lib/ruby-wmi.rb
@@ -1,7 +1,3 @@
-class RubyWMI
-  VERSION = '0.3.0'
-end
-
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
